3b7d53eee3f8170aa42553e8b0d68df6c08797dc
Javier Torrecilla Reyes <124880910+javiertorrecilla@users.noreply.github.com>
2024-03-14 18:57:46 +0100

    Update ClubDeportivoTest.java

--- practica1/src/test/java/clubdeportivo/ClubDeportivoTest.java
+++ practica1/src/test/java/clubdeportivo/ClubDeportivoTest.java
@@ -2,6 +2,7 @@ package clubdeportivo;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -9,33 +10,12 @@ import org.junit.jupiter.api.Test;
 public class ClubDeportivoTest {
     
     @Test
-    @DisplayName("Comprueba que dos clubes deportivos son iguales si su nombre es el mismo y el numero de grupos tambien lo es")
-    public void ClubDeportivo_CompareNoGroups_CheckEquals() throws ClubException{
+    @DisplayName("Comprueba que se crea el constructor de la manera correcta")
+    public void ClubDeportivo_WithTam_CheckCreation() throws ClubException{
         String nombre = "ClubDeportivo";
         int tam = 3;
         ClubDeportivo cd1 = new ClubDeportivo(nombre, tam);
         String expectedValue = "ClubDeportivo --> [  ]";
-        String returnValue = cd1.toString();
-        assertEquals(expectedValue, returnValue);
-    }
-
-    @Test
-    @DisplayName("Comprueba que dos clubes deportivos son iguales si su nombre es el mismo y el numero de grupos tambien lo es")
-    public void ClubDeportivo_CompareWithGroups_CheckEquals() throws ClubException{
-        String nombre = "ClubDeportivo";
-        int tam = 3;
-        ClubDeportivo cd1 = new ClubDeportivo(nombre, tam);
-
-        String codigo = "GrupoUno";
-        String actividad = "Yoga";
-        int nplazas = 10;
-        int matriculados =  5;
-        double tarifa =  10;
-        Grupo grupo = new Grupo(codigo, actividad, nplazas, matriculados, tarifa);
-
-        cd1.anyadirActividad(grupo);
-
-        String expectedValue = "ClubDeportivo --> [ (GrupoUno - Yoga - 10.0 euros - P:10 - M:5) ]";
         String returnValue = cd1.toString();
         assertEquals(expectedValue, returnValue);
     }
@@ -46,23 +26,13 @@ public class ClubDeportivoTest {
         String nombre1 = "ClubDeportivo";
         String nombre2 = "ClubBaloncesto";
         int tam = 3;
-        ClubDeportivo cd1 = new ClubDeportivo(nombre1, tam);
+        ClubDeportivo cd1 = new ClubDeportivo(nombre1);
         ClubDeportivo cd2 = new ClubDeportivo(nombre2, tam);
         String expectedValue = cd1.toString();
         String returnValue = cd2.toString();
         assertNotEquals(expectedValue, returnValue);
     }
 
-    @Test
-    @DisplayName("Comprueba que dos clubes deportivos son iguales si su nombre es el mismo y el numero de grupos tambien lo es")
-    public void ClubDeportivo_CompareName_CheckEquals() throws ClubException{
-        String nombre = "ClubDeportivo";
-        ClubDeportivo cd1 = new ClubDeportivo(nombre);
-        ClubDeportivo cd2 = new ClubDeportivo(nombre);
-        String expectedValue = cd1.toString();
-        String returnValue = cd2.toString();
-        assertEquals(expectedValue, returnValue);
-    }
 
     @Test
     @DisplayName("Comprueba que se lanza un error si se crea un club con 0 o menos grupos")
@@ -72,19 +42,21 @@ public class ClubDeportivoTest {
         assertThrows(ClubException.class, () -> ClubDeportivo cd = new ClubDeportivo(nombre, tam));
     }
 
-    /*@Test
+    @Test
     @DisplayName("Comprueba que se añade una actividad dado un string valido")
     public void anyadirActividad_StringDatos_Check() throws ClubException{
         String nombre = "ClubDeportivo";
         int tam1 = 3;
         ClubDeportivo club = new ClubDeportivo(nombre, tam1);
         
-        String[] datos = {"GrupoUno", "Yoga", "20", "15", "10.50"}; 
+        String[] datos = {"GrupoUno", "Yoga", "20", "15", "10"}; 
 
         club.anyadirActividad(datos);
 
-        //Falta assert
-    }*/
+        String expectedValue = "ClubDeportivo --> [ (GrupoUno - Yoga - 10.0 euros - P:20 - M:15) ]";
+        String returnValue = club.toString();
+        assertEquals(expectedValue, returnValue);
+    }
 
     @Test
     @DisplayName("Comprueba que se lanza un error debido a que no se insertan los datos de formato correcto")
@@ -146,24 +118,26 @@ public class ClubDeportivoTest {
         assertEquals(expectedValue, returnValue);
     }
 
-    /*@Test
-    @DisplayName("Comprueba que se actualiza las plazas de un grupo ya existente")
-    public void anyadirActividad_NewGroup_Check() throws ClubException{
+    @Test
+    @DisplayName("Comprueba que se añade una actividad a un club dado un grupo de la misma")
+    public void anyadirActividad_Check() throws ClubException{
         String nombre = "ClubDeportivo";
-        int tam1 = 3;
-        ClubDeportivo club = new ClubDeportivo(nombre, tam1);
-        
+        int tam = 3;
+        ClubDeportivo cd1 = new ClubDeportivo(nombre, tam);
+
         String codigo = "GrupoUno";
         String actividad = "Yoga";
         int nplazas = 10;
         int matriculados =  5;
         double tarifa =  10;
-        Grupo grupo1 = new Grupo(codigo, actividad, nplazas, matriculados, tarifa);
+        Grupo grupo = new Grupo(codigo, actividad, nplazas, matriculados, tarifa);
 
-        club.anyadirActividad(grupo1);
+        cd1.anyadirActividad(grupo);
 
-        //Falta assert
-    }*/
+        String expectedValue = "ClubDeportivo --> [ (GrupoUno - Yoga - 10.0 euros - P:10 - M:5) ]";
+        String returnValue = cd1.toString();
+        assertEquals(expectedValue, returnValue);
+    }
 
     @Test
     @DisplayName("Comprueba que las plazas libres de una actividad cuando hay mas de un grupo de dicha actividad")
@@ -236,6 +210,70 @@ public class ClubDeportivoTest {
         int expectedValue = 0;
         int returnValue = club.plazasLibres("Pilates");
 
+        assertEquals(expectedValue, returnValue);
+    }
+
+    @Test
+    @DisplayName("Comprueba que salta un error cuando se intenta matricular a más personas de las plazas libres que quedan en una actividad")
+    public void matricular_LessPlazas_ThrowsError() throws ClubException{
+        String nombre = "ClubDeportivo";
+        int tam1 = 3;
+        ClubDeportivo club = new ClubDeportivo(nombre, tam1);
+        
+        String codigo = "GrupoUno";
+        String actividad = "Yoga";
+        int nplazas = 10;
+        int matriculados =  5;
+        double tarifa =  10;
+        Grupo grupo1 = new Grupo(codigo, actividad, nplazas, matriculados, tarifa);
+
+        club.anyadirActividad(grupo1);
+
+        assertThrows(ClubException.class, () -> club.matricular(grupo1.getActividad(), 10));
+    }
+
+    @Test
+    @DisplayName("Comprueba que no se actualizan las matriculaciones de un grupo correctamente")
+    public void matricular__Check() throws ClubException{
+        String nombre = "ClubDeportivo";
+        int tam1 = 3;
+        ClubDeportivo club = new ClubDeportivo(nombre, tam1);
+        
+        String codigo = "GrupoUno";
+        String actividad = "Yoga";
+        int nplazas = 10;
+        int matriculados =  5;
+        double tarifa =  10;
+        Grupo grupo1 = new Grupo(codigo, actividad, nplazas, matriculados, tarifa);
+
+        club.anyadirActividad(grupo1);
+
+        int nuevos = 3;
+
+        int expectedValue = matriculados+nuevos;
+        club.matricular(grupo1.getActividad(), nuevos);
+        int returnValue = grupo1.getMatriculados();
+        assertEquals(expectedValue, returnValue);
+    }
+
+    @Test
+    @DisplayName("Comprueba que no se actualizan las matriculaciones de un grupo correctamente")
+    public void ingresos_Check() throws ClubException{
+        String nombre = "ClubDeportivo";
+        int tam1 = 3;
+        ClubDeportivo club = new ClubDeportivo(nombre, tam1);
+        
+        String codigo = "GrupoUno";
+        String actividad = "Yoga";
+        int nplazas = 10;
+        int matriculados =  5;
+        double tarifa =  10;
+        Grupo grupo1 = new Grupo(codigo, actividad, nplazas, matriculados, tarifa);
+
+        club.anyadirActividad(grupo1);
+
+        double expectedValue = matriculados*tarifa;
+        double returnValue = club.ingresos();
         assertEquals(expectedValue, returnValue);
     }
 
