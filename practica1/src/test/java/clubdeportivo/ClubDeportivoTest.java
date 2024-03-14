1b52284d3d4ffea24df8a6dbf5be1ef0f4f365c6
Javier Torrecilla Reyes <124880910+javiertorrecilla@users.noreply.github.com>
2024-03-14 15:38:17 +0100

    Update ClubDeportivoTest.java

--- practica1/src/test/java/clubdeportivo/ClubDeportivoTest.java
+++ practica1/src/test/java/clubdeportivo/ClubDeportivoTest.java
@@ -33,19 +33,160 @@ public class ClubDeportivoTest {
         assertNotEquals(expectedValue, returnValue);
     }
 
-    @Test
-    @DisplayName("Comprueba que dos clubes deportivos no son iguales porque no tienen el mismo numero de grupos")
-    public void ClubDeportivo_Compare_DifferentGroups() throws ClubException{
+    /*@Test
+    @DisplayName("Comprueba que se añade una actividad dado un string valido")
+    public void anyadirActividad_StringDatos_Check() throws ClubException{
         String nombre = "ClubDeportivo";
         int tam1 = 3;
-        int tam2 = 5;
-        ClubDeportivo cd1 = new ClubDeportivo(nombre, tam1);
-        ClubDeportivo cd2 = new ClubDeportivo(nombre, tam2);
-        String expectedValue = cd1.toString();
-        String returnValue = cd2.toString();
-        assertNotEquals(expectedValue, returnValue);
+        ClubDeportivo club = new ClubDeportivo(nombre, tam1);
+        
+        String[] datos = {"GrupoUno", "Yoga", "20", "15", "10.50"}; 
+
+        club.anyadirActividad(datos);
+
+        //Falta assert
+    }*/
+
+    @Test
+    @DisplayName("Comprueba que se lanza un error debido a que no se insertan los datos de formato correcto")
+    public void anyadirActividad_StringDatos_ThrowsError() throws ClubException{
+        String nombre = "ClubDeportivo";
+        int tam1 = 3;
+        ClubDeportivo club = new ClubDeportivo(nombre, tam1);
+        
+        String[] datos = {"GrupoUno", "Yoga", "Veinte", "15", "10.50"}; 
+
+        assertThrows(NumberFormatException.class,() -> club.anyadirActividad(datos));
+    }
+
+    @Test
+    @DisplayName("Comprueba que se lanza un error debido a que se inserta un grupo nulo")
+    public void anyadirActividad_NullGroup_ThrowsError() throws ClubException{
+        String nombre = "ClubDeportivo";
+        int tam1 = 3;
+        ClubDeportivo club = new ClubDeportivo(nombre, tam1);
+        
+        String codigo = "GrupoUno";
+        String actividad = "Yoga";
+        int nplazas = 10;
+        int matriculados =  5;
+        double tarifa =  10;
+        Grupo grupo = new Grupo(codigo, actividad, nplazas, matriculados, tarifa);
+
+        assertThrows(ClubException.class,() -> club.anyadirActividad(null));
+    }
+
+    @Test
+    @DisplayName("Comprueba que se actualiza las plazas de un grupo ya existente")
+    public void anyadirActividad_ExistingGroup_UpdatePlazas() throws ClubException{
+        String nombre = "ClubDeportivo";
+        int tam1 = 3;
+        ClubDeportivo club = new ClubDeportivo(nombre, tam1);
+        
+        String codigo = "GrupoUno";
+        String actividad = "Yoga";
+        int nplazas = 10;
+        int matriculados =  5;
+        double tarifa =  10;
+        Grupo grupo1 = new Grupo(codigo, actividad, nplazas, matriculados, tarifa);
+
+        club.anyadirActividad(grupo1);
+
+        String cod = "GrupoUno";
+        String act = "Yoga";
+        int plazas = 20;
+        int matriculas =  5;
+        double coste =  10;
+        Grupo grupo2 = new Grupo(cod, act, plazas, matriculas, coste);
+
+        club.anyadirActividad(grupo2);
+
+        int expectedValue = 20;
+        int returnValue = grupo2.getPlazas();
+
+        assertEquals(expectedValue, returnValue);
+    }
+
+    /*@Test
+    @DisplayName("Comprueba que se actualiza las plazas de un grupo ya existente")
+    public void anyadirActividad_NewGroup_Check() throws ClubException{
+        String nombre = "ClubDeportivo";
+        int tam1 = 3;
+        ClubDeportivo club = new ClubDeportivo(nombre, tam1);
+        
+        String codigo = "GrupoUno";
+        String actividad = "Yoga";
+        int nplazas = 10;
+        int matriculados =  5;
+        double tarifa =  10;
+        Grupo grupo1 = new Grupo(codigo, actividad, nplazas, matriculados, tarifa);
+
+        club.anyadirActividad(grupo1);
+
+        //Falta assert
+    }*/
+
+    @Test
+    @DisplayName("Comprueba que las plazas libres de una actividad cuando hay mas de un grupo de dicha actividad")
+    public void plazasLibres_MoreThanOneGroup_Check() throws ClubException{
+        String nombre = "ClubDeportivo";
+        int tam1 = 3;
+        ClubDeportivo club = new ClubDeportivo(nombre, tam1);
+        
+        String codigo = "GrupoUno";
+        String actividad = "Yoga";
+        int nplazas = 10;
+        int matriculados =  5;
+        double tarifa =  10;
+        Grupo grupo1 = new Grupo(codigo, actividad, nplazas, matriculados, tarifa);
+
+        String codigo2 = "GrupoDos";
+        String actividad2 = "Yoga";
+        int nplazas2 = 10;
+        int matriculados2 =  5;
+        double tarifa2 =  10;
+        Grupo grupo2 = new Grupo(codigo2, actividad2, nplazas2, matriculados2, tarifa2);
+
+        club.anyadirActividad(grupo1);
+        club.anyadirActividad(grupo2);
+
+        int expectedValue = grupo1.plazasLibres()+grupo2.plazasLibres();
+        int returnValue = club.plazasLibres(grupo1.getActividad());
+
+        assertEquals(expectedValue, returnValue);
+    }
+
+    @Test
+    @DisplayName("Comprueba que las plazas libres de una actividad cuando solo hay un grupo de dicha actividad")
+    public void plazasLibres_OnlyOneGroup_Check() throws ClubException{
+        String nombre = "ClubDeportivo";
+        int tam1 = 3;
+        ClubDeportivo club = new ClubDeportivo(nombre, tam1);
+        
+        String codigo = "GrupoUno";
+        String actividad = "Yoga";
+        int nplazas = 10;
+        int matriculados =  5;
+        double tarifa =  10;
+        Grupo grupo1 = new Grupo(codigo, actividad, nplazas, matriculados, tarifa);
+
+        club.anyadirActividad(grupo1);
+
+        int expectedValue = grupo1.plazasLibres();
+        int returnValue = club.plazasLibres(grupo1.getActividad());
+
+        assertEquals(expectedValue, returnValue);
     }
 
 
 
+
+
+
+
+
+
+
+
 }
+
