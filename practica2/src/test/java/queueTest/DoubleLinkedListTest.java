0c50d9f0b10a6c3bcba735096fa0fffea60e6ac1
sandravazquezpe <124881035+sandravazquezpe@users.noreply.github.com>
2024-03-19 11:25:55 +0100

    inicio Practica 2

---
+++ practica2/src/test/java/queueTest/DoubleLinkedListTest.java
@@ -0,0 +1,5 @@
+package queueTest;
+
+public class DoubleLinkedListTest {
+    
+}
---
+++ practica2/src/test/java/queueTest/LinkedNodeTest.java
@@ -0,0 +1,8 @@
+package queueTest;
+
+import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class LinkedNodeTest {
+
+}
