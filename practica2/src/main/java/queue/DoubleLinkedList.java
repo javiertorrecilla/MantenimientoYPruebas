2ea68e21036367c9c961f348c6ccee7cce99cb5f
sandravazquezpe <124881035+sandravazquezpe@users.noreply.github.com>
2024-03-19 19:47:01 +0100

    Update DoubleLinkedList.java

--- practica2/src/main/java/queue/DoubleLinkedList.java
+++ practica2/src/main/java/queue/DoubleLinkedList.java
@@ -1,50 +1,88 @@
 package queue;
 
 public class DoubleLinkedList<T> implements DoubleLinkedQueue<T> {
-
     private LinkedNode<T> first;
     private LinkedNode<T> last;
     private int size;
 
     public DoubleLinkedList() {
-        // TODO
+        first = null;
+        last = null;
+        size = 0;
     }
 
     @Override
     public void prepend(T value) {
-        // TODO
+        LinkedNode<T> node = new LinkedNode<>(value, null, first);
+        if (first == null) {
+            first = node;
+            last = node;
+        } else {
+            first.setPrevious(node);
+            node.setNext(first);
+            first = node;
+        }
+        size++;
     }
 
     @Override
     public void append(T value) {
-        // TODO
+        LinkedNode<T> node = new LinkedNode<>(value, last, null);
+        if (last == null) {
+            first = node;
+            last = node;
+        } else {
+            last.setNext(node);
+            node.setPrevious(last);
+            last = node;
+        }
+        size++;
     }
 
     @Override
     public void deleteFirst() {
-        // TODO
+        if (first != null) {
+            first = first.getNext();
+            if (first != null) {
+                first.setPrevious(null);
+            } else {
+                last = null;
+            }
+            size--;
+        }
     }
 
     @Override
     public void deleteLast() {
-        // TODO
+        if (last != null) {
+            last = last.getPrevious();
+            if (last != null) {
+                last.setNext(null);
+            } else {
+                first = null;
+            }
+            size--;
+        }
     }
 
     @Override
     public T first() {
-        // TODO
+        if (first != null) {
+            return first.getItem();
+        }
         return null;
     }
 
     @Override
     public T last() {
-        // TODO
+        if (last != null) {
+            return last.getItem();
+        }
         return null;
     }
 
     @Override
     public int size() {
-        // TODO
-        return 0;
+        return size;
     }
 }
