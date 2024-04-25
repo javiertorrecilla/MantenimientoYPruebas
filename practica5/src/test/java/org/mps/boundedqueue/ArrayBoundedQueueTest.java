2c754be4c826cc9f1fc14f77b4f1187fd2cf7b03
sandravazquezpe <124881035+sandravazquezpe@users.noreply.github.com>
2024-04-25 11:25:59 +0200

    Update ArrayBoundedQueueTest.java

--- practica5/src/test/java/org/mps/boundedqueue/ArrayBoundedQueueTest.java
+++ practica5/src/test/java/org/mps/boundedqueue/ArrayBoundedQueueTest.java
@@ -5,9 +5,17 @@ import static org.assertj.core.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 public class ArrayBoundedQueueTest {
+
+    ArrayBoundedQueue arrayBoundedQueue;
     
     @Test
-    public void prueba(){
-        assertThat(1).isEqualTo(1);
+    public void ArrayBoundedQueue_capacityMenorIgualCero_ThrowsError(){
+        int capacity = 0;
+        String message = "ArrayBoundedException: capacity must be positive";
+        assertThatExceptionOfType(IllegalArgumentException.class)
+        .isThrownBy(()->{
+            arrayBoundedQueue = new ArrayBoundedQueue<>(capacity);
+        })
+        .withMessage(message);
     }
 }
