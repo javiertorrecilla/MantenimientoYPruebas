55e29f982198c62b2db7c6519c4abc9c6041c6e0
sandravazquezpe <124881035+sandravazquezpe@users.noreply.github.com>
2024-04-25 11:06:46 +0200

    creacion Practica 5

---
+++ practica5/src/test/java/org/mps/boundedqueue/ArrayBoundedQueueTest.java
@@ -0,0 +1,13 @@
+package org.mps.boundedqueue;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+public class ArrayBoundedQueueTest {
+    
+    @Test
+    public void prueba(){
+        assertThat(1).isEqualTo(1);
+    }
+}
