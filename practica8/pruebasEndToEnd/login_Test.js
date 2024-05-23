4454f09e6af57d85f5d4e1430dd9311556d1de39
sandravazquezpe <sandravazquezpe@uma.es>
2024-05-23 11:48:25 +0200

    LOGIN_TEST.JS

---
+++ practica8/pruebasEndToEnd/login_Test.js
@@ -0,0 +1,38 @@
+import { browser } from 'k6/experimental/browser';
+import { check, sleep } from 'k6';
+
+export const options = {
+    scenarios: {
+      ui: {
+        executor: 'shared-iterations', // para realizar iteraciones sin indicar el tiempo
+        options: {
+          browser: {
+            type: 'chromium',
+          },
+        },
+      } 
+    },
+    thresholds: {
+      checks: ["rate==1.0"]
+    }
+  }
+
+export default async function () {
+    const page = browser.newPage();
+    try {
+    await page.goto('http://localhost:4200/');
+
+    page.locator('input[name="nombre"]').type('Javier');
+    page.locator('input[name="DNI"]').type('11111111X');
+
+    const submitButton = page.locator('input[name="login"]');
+
+    await Promise.all([page.waitForNavigation(), submitButton.click()]);
+
+    check(page, {
+    'header': p => p.locator('h2').textContent() == 'Listado de pacientes',
+    });
+    } finally {
+        page.close();
+    }
+}
